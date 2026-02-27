9200d37c30c69c4b26ae96a554103d27d08c0fb2
vboxuser <vboxuser@WIN10>
2026-02-27 10:03:57 +0300

    Добавьте файлы проекта.

---
+++ BookMaster_34/View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using BookMaster_34.View.Pages;
+using BookMaster_34.View.Windows;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookMaster_34
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MainFrame.Navigate(new BrowseCatalogPage());
+        }
+
+        private void LogoutMi_Click(object sender, RoutedEventArgs e)
+        {
+           LogoutWindow logoutWindow = new LogoutWindow();
+            //logoutWindow.ShowDialog();
+
+            LibraryMi.Visibility = Visibility.Collapsed;
+            LogoutMi.Visibility = Visibility.Collapsed;
+            LoginMi.Visibility = Visibility.Visible;
+        }
+
+        private void LoginMi_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+          
+
+            if (loginWindow.ShowDialog() == true)
+            {
+                LibraryMi.Visibility = Visibility.Collapsed;
+                LogoutMi.Visibility = Visibility.Visible;
+                LoginMi.Visibility = Visibility.Collapsed;
+            }
+
+        }
+
+        private void CloseAppMi_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+
+        }
+
+        private void ManageCustomersMi_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ManageCustomersPage());
+        }
+
+        private void ReportsMi_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ReportsPage());
+        }
+
+        private void CirculationMi_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new CirculationPage());
+        }
+
+        private void CatalogMi_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new BrowseCatalogPage());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+🔞 Только для меня и Отделянова В. В. 🔞
+
+(Загрузка пакетов)
+Install-Package Microsoft.EntityFrameworkCore -version 9.0.0 
+Install-Package Microsoft.EntityFrameworkCore.Tools -version 9.0.0
+Install-Package Microsoft.EntityFrameworkCore.Design -version 9.0.0
+Install-Package Microsoft.EntityFrameworkCore.Proxies -version 9.0.0
+Install-Package Microsoft.EntityFrameworkCore.SqlServer -version 9.0.0
+
+ Scaffold-DbContext "Server=.\SQLEXPRESS01;Database=Bookmaster;Trusted_Connection=true;TrustServerCertificate=true" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
